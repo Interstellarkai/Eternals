31e4a74715d809c8031291682d846124d8b90cb1
CredoSpero <yixuan1402@gmail.com>
2022-08-09 17:08:17 +0800

    embedded dashboards into our home page for client and banker

--- frontend/src/components/Client/ClientHome.js
+++ frontend/src/components/Client/ClientHome.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Navbar from '../Navbar';
+import Menu from "./Menu"
+
 const ClientHome = () => {
+
+	const [pageName, setPageName] = useState("Citibank");
+
+	useEffect(() => {
+		const container = document.getElementById("content-container")
+
+		window.microstrategy.dossier.create({
+			url:"https://env-262122.customer.cloud.microstrategy.com/MicroStrategyLibrary/app/B7CA92F04B9FAE8D941C3E9B7E0CD754/134BC2D5C1443891388CEF8C9B640660/W352--K190",
+			enableResponsive: true,
+			containerHeight: '1000px',
+			placeholder: container
+		}).then((dossier) => {
+			dossier.registerEventHandler(window.microstrategy.dossier.EventType.ON_PAGE_SWITCHED, updateTitle);
+			window.dossier = dossier
+			window.menu.updateDossier()
+			updateTitle()
+		}); 
+	});
+
+	const updateTitle = (data) => {
+		const currCh = window.dossier.children[window.dossier.currentChapterIndex]
+		const currPage = currCh.children[currCh.currentPageIndex]
+		
+		console.log("In update title, currPage: ", currPage.name);
+		// setPageName(currPage.name)
+	}
+
 	return (
 		<>
 			<Navbar />
+
+			<div className="grid-container" id="grid-container">
+				<div className="header-container">
+					<h2>{"MicroStrategy Embedding SDK Example - "}{pageName}</h2>
+				</div>
+				<Menu></Menu>
+				<div className="content-container" id="content-container">
+					
+				</div>
+
+			</div>
 		</>
 	);
 };
