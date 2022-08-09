523da10576dcdec81a22f95e6aa9a0dfffba5f63
bbobohang <bbobohang@gmail.com>
2022-08-09 23:56:44 +0800

    Changed acad

--- frontend/src/components/Client/ClientAcad.js
+++ frontend/src/components/Client/ClientAcad.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './ClientAcad.css';
 import { Link } from 'react-router-dom';
+import AcadCard from './AcadCard';
 const ClientAcad = () => {
 	const [slide, setSlide] = useState(0);
 	return (
@@ -9,27 +10,19 @@ const ClientAcad = () => {
 				<Link to='/client/home'>
 					<div className='logo-left'></div>
 				</Link>
-				<div className='logo-right'>Citi Acadmeny</div>
+				<div className='logo-right'>Citi Academy</div>
 			</div>
 			<div className='acad-wrapper'>
-				<div className={slide === 0 ? 'acad-slide active' : 'acad-slide'}>
-					Slide 1
+				<h2>Wealth Management Make Easy</h2>
+				<p>
+					Learn from our interactive courses to increase your finanical literacy
+				</p>
+				<div className='acad-content'>
+					<AcadCard />
+					<AcadCard />
+					<AcadCard />
+					<AcadCard />
 				</div>
-				<div className={slide === 1 ? 'acad-slide active' : 'acad-slide'}>
-					Slide 2
-				</div>
-				<div className={slide === 2 ? 'acad-slide active' : 'acad-slide'}>
-					Slide 3
-				</div>
-				<div className={slide === 3 ? 'acad-slide active' : 'acad-slide'}>
-					Slide 4
-				</div>
-				<button className='next-button' onClick={() => setSlide(slide + 1)}>
-					Next
-				</button>
-				<button className='prev-button' onClick={() => setSlide(slide - 1)}>
-					Prev
-				</button>
 			</div>
 		</>
 	);
