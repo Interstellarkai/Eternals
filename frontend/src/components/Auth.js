f2e6ab434a89f1107e37c0f900e8366e3b2b27fa
bbobohang <bbobohang@gmail.com>
2022-08-09 13:25:34 +0800

    Added authpage

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,7 +1,36 @@
 import React from 'react';
-
+import './Auth.css';
+import { Link } from 'react-router-dom';
 const Auth = () => {
-	return <div>Auth</div>;
+	return (
+		<div className='auth-wrapper'>
+			<div className='bg-image'></div>
+			<div className='auth-content'>
+				<div className='auth-title'>
+					<h1>Welcome To Eternals</h1>
+					<p>
+						<br />
+						Your one stop solution for wealth management!
+					</p>
+				</div>
+				<div className='auth-subtitle'>
+					<h2>I am a...</h2>
+				</div>
+				<div className='content-wrapper'>
+					<div className='auth-left'>
+						<Link to='/client/home'>
+							<p className='auth-text'>Client</p>
+						</Link>
+					</div>
+					<div className='auth-right'>
+						<Link to='/banker/home'>
+							<p className='auth-text'>Banker</p>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Auth;
