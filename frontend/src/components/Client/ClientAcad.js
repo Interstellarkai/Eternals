c8f87dbfdfe4e7d0a1f6e89035a085261ad333cc
bbobohang <bbobohang@gmail.com>
2022-08-09 15:29:06 +0800

    Added acad

--- frontend/src/components/Client/ClientAcad.js
+++ frontend/src/components/Client/ClientAcad.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ClientAcad.css';
 import { Link } from 'react-router-dom';
 const ClientAcad = () => {
+	const [slide, setSlide] = useState(0);
 	return (
 		<>
 			<div className='top-nav'>
@@ -9,6 +10,26 @@ const ClientAcad = () => {
 					<div className='logo-left'></div>
 				</Link>
 				<div className='logo-right'>Citi Acadmeny</div>
+			</div>
+			<div className='acad-wrapper'>
+				<div className={slide === 0 ? 'acad-slide active' : 'acad-slide'}>
+					Slide 1
+				</div>
+				<div className={slide === 1 ? 'acad-slide active' : 'acad-slide'}>
+					Slide 2
+				</div>
+				<div className={slide === 2 ? 'acad-slide active' : 'acad-slide'}>
+					Slide 3
+				</div>
+				<div className={slide === 3 ? 'acad-slide active' : 'acad-slide'}>
+					Slide 4
+				</div>
+				<button className='next-button' onClick={() => setSlide(slide + 1)}>
+					Next
+				</button>
+				<button className='prev-button' onClick={() => setSlide(slide - 1)}>
+					Prev
+				</button>
 			</div>
 		</>
 	);
