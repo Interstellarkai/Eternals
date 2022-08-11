46b531f40dee2190100a58dcefc1fed8b25b7577
KaiSheng Lim <limkaisheng.work@gmail.com>
2022-08-11 15:09:16 +0800

    frontend with moderate risk

--- frontend/src/components/Banker/AssetCard.js
+++ frontend/src/components/Banker/AssetCard.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { SiNetflix } from 'react-icons/si';
-const AssetCard = () => {
+const AssetCard = ({ item }) => {
 	return (
 		<>
 			<div className='max-grid-content'>
 				<div className='asset-container'>
 					<div>
-						<h2>Netflix</h2>
+						<h2>{item.name} </h2>
+						<p>{item.ticker}</p>
 					</div>
 				</div>
-				<div className='asset-text'>1.33</div>
-				<div className='asset-text'>5.4x</div>
-				<div className='asset-text'>34%</div>
-				<div className='asset-text'>3.4</div>
+				<div className='asset-text'>{item.debt_to_equity}</div>
+				<div className='asset-text'>{item.interest_coverage}x</div>
+				<div className='asset-text'>{item.return_on_equity}%</div>
+				<div className='asset-text'>{item.PE_ratio}</div>
 			</div>
 		</>
 	);
--- frontend/src/components/Banker/BankerMax.js
+++ frontend/src/components/Banker/BankerMax.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './BankerMax.css';
 import Navbar from '../Navbar';
 import { useLocation } from 'react-router-dom';
@@ -7,6 +7,20 @@ import AssetCard from './AssetCard';
 const BankerMax = () => {
 	const { state } = useLocation();
 	console.log(state);
+	const [data, setData] = useState([])
+	useEffect(() => { 
+		fetch(`http://127.0.0.1:5000/moderaterisk`).then(
+			res => res.json()
+			
+		).then(
+			data=> {
+				setData(data)
+				console.log(data);
+			}
+		).catch((error)=>{
+			console.log(error);
+		})
+	},[])
 	return (
 		<>
 			<Navbar view='banker' />
@@ -24,10 +38,13 @@ const BankerMax = () => {
 					<div>ROE</div>
 					<div>P/E RATIO</div>
 				</div>
+				{data.map((item,index)=>(
+					<AssetCard item = {item} key={index}/>
+				))}
+				{/* <AssetCard />
 				<AssetCard />
 				<AssetCard />
-				<AssetCard />
-				<AssetCard />
+				<AssetCard /> */}
 			</div>
 		</>
 	);
